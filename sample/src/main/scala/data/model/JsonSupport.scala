262ca9738e8eda0af564b73e5f713ea4137ffe51
Tobias Frischholz <tfrischholz@kabelmail.de>
2018-03-04 23:38:57 +0100

    Updated json marshalling protocol

--- sample/src/main/scala/data/model/JsonSupport.scala
+++ sample/src/main/scala/data/model/JsonSupport.scala
@@ -1,16 +1,22 @@
 package data.model
 
 import java.sql.Timestamp
+import java.time.Instant
 import java.util.UUID
 
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
-import spray.json.{DefaultJsonProtocol, JsString, JsValue, JsonFormat, RootJsonFormat}
+import spray.json.{DefaultJsonProtocol, JsNumber, JsString, JsValue, JsonFormat, RootJsonFormat}
 
 trait BaseJsonProtocol extends DefaultJsonProtocol {
   implicit val timestampFormat: JsonFormat[Timestamp] = new JsonFormat[Timestamp] {
-    override def write(obj: Timestamp): JsValue = JsString(obj.toString)
+    override def write(obj: Timestamp): JsValue = JsNumber(obj.getTime)
 
-    override def read(json: JsValue): Timestamp = Timestamp.valueOf(json.toString())
+    override def read(json: JsValue): Timestamp = json match {
+      case JsNumber(x) => Timestamp.from(Instant.ofEpochMilli(x.toLong))
+      case _ =>
+        throw new IllegalArgumentException(
+          s"Can not parse json value [$json] to a timestamp object")
+    }
   }
 
   implicit val uuidJsonFormat: JsonFormat[UUID] = new JsonFormat[UUID] {
